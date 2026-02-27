11211cc1030e55bb45998903c712a25d4d22ef84
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2026-02-27 15:27:00 -0800

    feat: authentication components, context

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,42 @@
+import jwtDecode from "jwt-decode";
+import { useState, useEffect, createContext, useContext } from "react";
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        setUser(decoded);
+      } catch (err) {
+        console.log("Token is invalid or corrupted", err);
+        logout();
+      }
+    } else {
+      setUser(null);
+    }
+  }, [token]);
+
+  const login = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setToken(newToken);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setUser(null);
+  };
+
+  const contextItems = {
+    token,
+    user,
+    login,
+    logout,
+  };
+
+  return <AuthContext value={contextItems}>{children}</AuthContext>;
+};
