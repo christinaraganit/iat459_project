6f64aaa04ded1d704d2e6f3562cd01fca0da7595
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2026-02-28 23:07:43 -0800

    feat: route for search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout/Layout";
 import { Index } from "./pages/Index/Index";
+import { Search } from "./pages/Search/Search";
 import { Dashboard } from "./pages/Dashboard/Dashboard";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
@@ -16,6 +17,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Layout />}>
             <Route path="/" element={<Index />} />
+            <Route path="/search" element={<Search />} />
             {/* Protected routes */}
             <Route
               path="/dashboard"
---
+++ frontend/src/pages/Search/Search.js
@@ -0,0 +1,20 @@
+import { Fragment, useState, useEffect } from "react";
+import { Test } from "../../components/Test/Test";
+import TCGdex from "@tcgdex/sdk";
+export const Search = () => {
+  const tcgdex = new TCGdex("en");
+
+  const [card, setCard] = useState(null);
+  useEffect(() => {
+    const fetchCard = async () => {
+      const card = await tcgdex.card.get("swsh3-136");
+      setCard(card);
+    };
+    fetchCard();
+  }, []);
+  return (
+    <Fragment>
+      <h1>xx results for y</h1>
+    </Fragment>
+  );
+};
