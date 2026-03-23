2cd88480f2d3c7b3f0b86f91aa5aa128ec629f54
Nathan Lew <nlew655@gmail.com>
2026-03-23 00:56:00 -0700

    feat: handle listing deletion

--- frontend/src/pages/Listing/Listing.js
+++ frontend/src/pages/Listing/Listing.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getListingByID, deleteListingByID } from "../../api/listings";
 import TCGdex from "@tcgdex/sdk";
+import { Navigate, redirect } from "react-router";
 import { useAuthContext } from "../../context/AuthContext";
 
 export const Listing = () => {
@@ -10,26 +11,34 @@ export const Listing = () => {
   const tcgdex = new TCGdex("en");
   const { cardId } = useParams();
   const [card, setCard] = useState();
+  const [validListing, setValidListing] = useState(false);
+  const [activeOwner, setActiveOwner] = useState(false);
+  const [deleted, setDeleted] = useState(false);
 
   const listingQuery = useQuery({
     queryKey: ["listings", cardId],
     queryFn: async () => {
       const listing = await getListingByID(cardId);
+      if (listing) setValidListing(true);
       const card = await tcgdex.card.get(listing.cardId);
       console.log(card);
       setCard(card);
-      setValidListing(listing.seller && listing.seller.username !== null);
+      setActiveOwner(listing.seller && listing.seller.username !== null);
       return { ...listing, card };
     },
   });
 
-  useEffect(() => {
-    console.log("data", listingQuery.data);
-  }, [listingQuery.data]);
+  const handleDelete = () => {
+    deleteListingByID(listingQuery.data?._id, token).then(() => {
+      setDeleted(true);
+    });
+  };
 
-  const [validListing, setValidListing] = useState(false);
-
-  return (
+  return deleted ? (
+    <Navigate to="/" />
+  ) : !validListing ? (
+    <div>invalid listing</div>
+  ) : (
     <div>
       <h1>Listing Item {cardId}</h1>
       {card && (
@@ -41,7 +50,7 @@ export const Listing = () => {
       <div>
         <p>Condition: {listingQuery.data?.condition}</p>
         <p>Price: ${listingQuery.data?.price.toFixed(2)}</p>
-        {validListing ? (
+        {activeOwner ? (
           <a href={`/user/${listingQuery.data?.seller.username}`}>
             Seller:{" "}
             {listingQuery.data?.seller.displayName ? (
@@ -55,11 +64,7 @@ export const Listing = () => {
         )}
       </div>
       {(role === "admin" || listingQuery.data?.seller._id === user?.id) && (
-        <button
-          onClick={() => deleteListingByID(listingQuery.data?._id, token)}
-        >
-          Delete Listing
-        </button>
+        <button onClick={handleDelete}>Delete Listing</button>
       )}
     </div>
   );
